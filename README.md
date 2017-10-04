6e875299a5fb1fa7755fe6d3dad40e358fbc1f60
rradjabi <rradjabi@gmail.com>
2017-10-04 16:55:49 -0700

    added notes.sh v0.1, added help contents to readme.

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # Notes
 Note taking script
+
+Version: 0.1
+
+Notes program created by Ryan Radjabi
+   Usage:
+       "Edit today's notes:        'notes'"
+       "Print today's notes:       'notes p'"
+       "Edit yesterday's notes:    'notes 1 e'"
+       "Print yesterday's notes:   'notes 1'"
+       "List all notes:            'notes ls'"
+
+
---
+++ notes.sh
@@ -0,0 +1,77 @@
+#!/bin/sh
+DIR=~/Notes/
+PREFIX=$1
+DATE=`date +%Y-%m-%d`
+#echo $DATE
+EDIT=1
+LIST_DIRS=0
+
+
+if [ "$1" == "help" ]; then
+    echo Notes program created by Ryan Radjabi
+    echo    Usage:
+    echo        "Edit today's notes:        'notes'"
+    echo        "Print today's notes:       'notes p'"
+    echo        "Edit yesterday's notes:    'notes 1 e'"
+    echo        "Print yesterday's notes:   'notes 1'"
+    echo        "List all notes:            'notes ls'"
+    exit
+fi
+
+# Check that input is only numeric
+if [ -z `echo $1 | tr -d "[:digit:]"` ]; then
+#    echo "Input contains only numeric characters"
+    if [ $1 > 0 ]; then
+        DATE=`date -d "$DATE - $1 day" +%Y-%m-%d` # prints current date - x days
+        if [ $# == 2 ]; then
+            if [ $2 == "e" ]; then
+#                echo performing edit
+                EDIT=1
+            else
+#                echo listing notes
+                EDIT=0
+            fi # Check argument is 'e' for edit
+        else
+#            echo argment length is too short for edit
+            EDIT=0
+        fi # check argument length equals 2
+    fi # check if first argument is greater than 0
+    PREFIX=Daily
+    FILE=$DIR$PREFIX-$DATE
+else # input is alpha
+    if [ $1 == "p" ]; then # p for print
+        EDIT=0 
+        PREFIX=Daily
+    else
+        EDIT=1
+        PREFIX=$1
+    fi
+
+#    echo prefix: $PREFIX
+    FILE=$DIR$PREFIX-$DATE
+
+    if [ $1 = "ls" ]; then
+        EDIT=0
+        LIST_DIRS=1
+    fi
+
+fi # check on numeric/alpha argument 
+
+if [ $EDIT == 1 ]; then
+    if [ ! -f $FILE ]; then
+        touch $FILE
+        echo "== $PREFIX notes for $DATE ==" > $FILE 
+    fi
+    vim $FILE
+    ls $DIR
+else
+    if [ $LIST_DIRS != 1 ]; then
+        echo 
+        echo
+        echo ~~~~~~~~~~~~~~~~~~
+        cat $FILE
+    else
+        ls $DIR
+    fi
+fi
+
